ca38a53bf0bc5e9e39d278ffd4a83ec2a52aeec1
EgorPopelyaev <egor@parity.io>
2025-07-10 18:43:26 +0200

    check if there are no prdocs then skip the reordering

--- .github/scripts/release/release_lib.sh
+++ .github/scripts/release/release_lib.sh
@@ -94,6 +94,16 @@ reorder_prdocs() {
     printf "[+] ℹ️ Reordering prdocs:"
 
     VERSION=$(sed -E 's/^v([0-9]+\.[0-9]+\.[0-9]+).*$/\1/' <<< "$VERSION") #getting reed of the 'v' prefix
+
+    # Check if there are any prdoc/pr_*.prdoc files
+    shopt -s nullglob
+    files=(prdoc/pr_*.prdoc)
+    shopt -u nullglob
+    if [ ${#files[@]} -eq 0 ]; then
+        echo " No pr_*.prdoc files found, skipping."
+        return 0
+    fi
+
     mkdir -p "prdoc/$VERSION"
     mv prdoc/pr_*.prdoc prdoc/$VERSION
     git add -A
